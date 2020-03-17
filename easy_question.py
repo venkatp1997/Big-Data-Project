e1980c05fc2aec23a141c6e6fbdcb250cc9f0f3a
Abinash Sinha <abinashsinha330@gmail.com>
2020-03-17 01:09:34 -0500

    updated

--- easy_question.py
+++ easy_question.py
@@ -4,7 +4,6 @@ Package for answering easy business questions
 
 import argparse
 import redis
-import collision_redis_put
 import utils
 
 # constants used
@@ -16,7 +15,7 @@ r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 # define and parse the arguments passed
 parser = argparse.ArgumentParser(description='Utility to answer easy business questions')
 
-parser.add_argument('--city', default='NYC', help='city under consideration')
+parser.add_argument('--city', default='NYC', help='city under consideration', required=True)
 parser.add_argument('--avg_coll_time', default=utils.HOUR,
                     help='average number of collisions hourly/daily/monthly/yearly')
 parser.add_argument('--num_coll_time', nargs=2, default=[utils.HOUR, '17-18'],
@@ -30,31 +29,39 @@ args = parser.parse_args()
 
 result = None
 city = args.city
+
 # average number of collisions per hour/day/month
 avg_col_rate_type = args.avg_coll_time
-if avg_col_rate_type is not None:
-    if avg_col_rate_type == utils.HOUR:
-        result = 0
-        for i in range(0, 24):
-            result = result + int(r.get(utils.get_key(utils.TIME, city, utils.HOUR, i)))
-        result = result/24
-    elif avg_col_rate_type == utils.DAY:
-        result = 0
-        for i in range(0, 7):
-            result = result + int(r.get(utils.get_key(utils.TIME, city, utils.DAY, utils.get_day(i))))
-        result = result/7
-    elif avg_col_rate_type == utils.MONTH:
-        result = 0
-        for i in range(1, 13):
-            result = result + int(r.get(utils.get_key(utils.TIME, city, utils.MONTH, utils.get_month(i))))
-        result = result / 12
-    elif avg_col_rate_type == utils.YEAR:
-        result = 0
-        for i in range(0, 7):
-            result = result + int(r.get(utils.get_key(utils.TIME, city, utils.YEAR, utils.get_day(i))))
-        result = result / 7
-    else:
-        raise ValueError('Invalid type of rate: ', avg_col_rate_type)
 
-    print('Average number of collisions on {}ly basis is: {}'.format(avg_col_rate_type, result))
 
+def get_avg_col_rate():
+    global result, i
+    if avg_col_rate_type is not None:
+        if avg_col_rate_type == utils.HOUR:
+            result = 0
+            for i in range(0, 24):
+                result = result + int(r.get(utils.get_key(utils.TIME, city, utils.HOUR, i)))
+            result = result / 24
+        elif avg_col_rate_type == utils.DAY:
+            result = 0
+            for i in range(0, 7):
+                result = result + int(r.get(utils.get_key(utils.TIME, city, utils.DAY, utils.get_day(i))))
+            result = result / 7
+        elif avg_col_rate_type == utils.MONTH:
+            result = 0
+            for i in range(1, 13):
+                result = result + int(r.get(utils.get_key(utils.TIME, city, utils.MONTH, utils.get_month(i))))
+            result = result / 12
+        elif avg_col_rate_type == utils.YEAR:
+            result = 0
+            for i in range(utils.START_YEAR, utils.CURRENT_YEAR + 1):
+                result = result + int(r.get(utils.get_key(utils.TIME, city, utils.YEAR, i)))
+            result = result / (utils.CURRENT_YEAR - utils.START_YEAR + 1)
+        else:
+            raise ValueError('Invalid type of rate: ', avg_col_rate_type)
+
+        print('Average number of collisions on {}ly basis is: {}'.format(avg_col_rate_type, result))
+
+
+# average number of collisions in given hour/day/month
+get_avg_col_rate()
